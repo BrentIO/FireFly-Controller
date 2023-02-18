53c5e55142fc8da86c7aaf7909b5dc0cb236cdd9
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2023-02-18 13:34:05 -0500

    Added additional abstraction for hardware-specific configurations

--- common/hardwareDefinitions.h
+++ common/hardwareDefinitions.h
@@ -5,18 +5,17 @@
 #ifndef hardware_definitions_h
     #define hardware_definitions_h
 
-    #include <PCA95x5.h> // https://github.com/semcneil/PCA95x5
-
-
     /* Define hardware enumeration constants */
     #define ENUM_MODEL_IO_EXTENDER_PCA9995 0 /* PCA9555 */
     #define ENUM_MODEL_OUTPUT_CONTROLLER_PCA9685 0 /* PCA9685 */
+    #define ENUM_MODEL_EEPROM_EXTERNAL_24LCXXX 0 /* 24LC024 + Family */
+    #define ENUM_MODEL_TEMPERATURE_SENSOR_PCT2075 0 /* PCT2075 */
 
     /* Hardware Types */
     #if PRODUCT_ID == 32322211
 
         #define SUPPORTED_HARDWARE
-
+       
         /* Input IO Extender */
         #define COUNT_IO_EXTENDER 8 /* The number of IO extenders. */
         #define MODEL_IO_EXTENDER ENUM_MODEL_IO_EXTENDER_PCA9995 /* IO Extender Model. */
@@ -26,6 +25,11 @@
         #define COUNT_OUTPUT_CONTROLLER 2 /* The number of output controllers. */
         #define MODEL_OUTPUT_CONTROLLER ENUM_MODEL_OUTPUT_CONTROLLER_PCA9685
         #define COUNT_PINS_OUTPUT_CONTROLLER 16 /* The number of pins on each output controller. */
+
+        /* External EEPROM */
+        #define MODEL_EEPROM_EXTERNAL ENUM_MODEL_EEPROM_EXTERNAL_24LCXXX
+        #define SIZE_EEPROM 256 /* Six of the external EEPROM.  NOTE: Divide the device size in Kbits by 8. */
+
         /* Temperature Sensor */
         #define COUNT_TEMPERATURE_SENSOR 1 /* The number of temperature sensors. */
         #define MODEL_TEMPERATURE_SENSOR ENUM_MODEL_TEMPERATURE_SENSOR_PCT2075
@@ -46,8 +50,6 @@
         #define ADDRESS_EEPROM 0x50 /* I2C addresses of the external EEPROM. */
         #define ADDRESS_OLED 0x3C /* I2C addresses of the OLED Display.  NOTE: This can be modifed on the display itself, default is 0x3C. */
 
-        #define SIZE_EEPROM 256 /* Six of the external EEPROM.  NOTE: Divide the device size in Kbits by 8. */
-
     #endif
 
     #ifndef SUPPORTED_HARDWARE
@@ -56,82 +58,104 @@
 
     #endif
 
-
     const uint8_t PINS_IO_EXTENDER[] = _PINS_INTERRUPT_IO_EXTENDER;
     const uint8_t ADDRESSES_IO_EXTENDER[] = _ADDRESSES_IO_EXTENDER;
     const uint8_t ADDRESSES_OUTPUT_CONTROLLER[] = _ADDRESSES_OUTPUT_CONTROLLER;
     const uint8_t ADDRESSES_TEMPERATURE_SENSORS[] = _ADDRESSES_TEMPERATURE_SENSOR;
 
 
- typedef struct {
-    char uuid[37];
-    char product_id[33];
-    char key[65];
-  } deviceType;
+    typedef struct {
+        char uuid[37];
+        char product_id[33];
+        char key[65];
+    } deviceType;
 
 
-  enum inputType{
-      NORMALLY_OPEN = 0,
-      NORMALLY_CLOSED = 1,
-  };
+    enum inputType{
+        NORMALLY_OPEN = 0,
+        NORMALLY_CLOSED = 1,
+    };
 
 
-  enum inputState{
-      STATE_OPEN = LOW,
-      STATE_CLOSED = HIGH
-  };
+    enum inputState{
+        STATE_OPEN = LOW,
+        STATE_CLOSED = HIGH
+    };
 
 
-  enum outputState{
-      STATE_HIGH = HIGH,
-      STATE_LOW = LOW
-  };
+    enum outputState{
+        STATE_HIGH = HIGH,
+        STATE_LOW = LOW
+    };
 
 
-  enum outputType{
-      BINARY = 0,
-      VARIABLE = 1
-  };
+    enum outputType{
+        BINARY = 0,
+        VARIABLE = 1
+    };
+
+    enum temperatureSensorLocation{
+        UNKNOWN = -1,
+        CENTER = 0
+    };
 
 
-  struct inputPin{
-      unsigned long timePreviousChange = 0; /* Time (millis) when the input state last changed.  Value is set to 0 when the state returns to its input type. Default 0.*/
-      inputState state = STATE_OPEN; /* The state entered at timePreviousChange. Default STATE_OPEN.*/
-      inputType type = NORMALLY_OPEN; /* Defines if the input is normally open or normally closed. Default NORMALLY_OPEN.*/
-      boolean monitorLongChange = false; /* Defines if the pin should be monitored for long changes.  Should be _true_ for buttons and _false_ for reed switches. Default false.*/
-  };
+    struct inputPin{
+        unsigned long timePreviousChange = 0; /* Time (millis) when the input state last changed.  Value is set to 0 when the state returns to its input type. Default 0.*/
+        inputState state = STATE_OPEN; /* The state entered at timePreviousChange. Default STATE_OPEN.*/
+        inputType type = NORMALLY_OPEN; /* Defines if the input is normally open or normally closed. Default NORMALLY_OPEN.*/
+        boolean monitorLongChange = false; /* Defines if the pin should be monitored for long changes.  Should be _true_ for buttons and _false_ for reed switches. Default false.*/
+    };
 
 
-  struct outputPin{
-      outputState state = STATE_LOW;
-      outputType type = BINARY;
-  };
+    struct outputPin{
+        outputState state = STATE_LOW;
+        outputType type = BINARY;
+    };
 
 
-  /* Input-specifc structures */
-  #if MODEL_IO_EXTENDER == ENUM_MODEL_IO_EXTENDER_PCA9555
+    /* Include hardware-specific libraries and set hardware-specific strucutres */
+    #if MODEL_IO_EXTENDER == ENUM_MODEL_IO_EXTENDER_PCA9995
+        #include <PCA95x5.h> // https://github.com/semcneil/PCA95x5
 
-      #define DEBOUNCE_DELAY 500 /* Milliseconds between changes for debouncing. */
+        #define DEBOUNCE_DELAY 500 /* Milliseconds between changes for debouncing. */
 
-      struct ioExtender{
-          PCA9555 hardware; /* Reference to the hardware. */
-          uint8_t interruptPin = 0; /* Interrupt pin. Default 0. */
-          uint8_t address = 0; /* I2C address. Default 0.*/
-          uint16_t previousRead = 0; /* Numeric value of the last read from the hardware. Default 0.*/
-          inputPin inputs[COUNT_PINS_IO_EXTENDER]; /* Input pins connected to the hardware.*/
-      };
+        struct ioExtender{
+            PCA9555 hardware; /* Reference to the hardware. */
+            uint8_t interruptPin = 0; /* Interrupt pin. Default 0. */
+            uint8_t address = 0; /* I2C address. Default 0.*/
+            uint16_t previousRead = 0; /* Numeric value of the last read from the hardware. Default 0.*/
+            inputPin inputs[COUNT_PINS_IO_EXTENDER]; /* Input pins connected to the hardware.*/
+        };
 
-  #endif
+    #endif
 
+    #if MODEL_OUTPUT_CONTROLLER == ENUM_MODEL_OUTPUT_CONTROLLER_PCA9685
+        #include <PCA9685.h> // https://github.com/RobTillaart/PCA9685_RT
 
-  /* Output-specifc structures */
-  #if MODEL_OUTPUT_CONTROLLER == ENUM_MODEL_OUTPUT_CONTROLLER_PCA9685
+        struct outputController{
+            uint8_t address = 0; /* I2C address. Default 0.*/
+            outputPin outputs[COUNT_PINS_OUTPUT_CONTROLLER];
+        };
 
-      struct outputController{
-          uint8_t address = 0; /* I2C address. Default 0.*/
-          outputPin outputs[COUNT_PINS_OUTPUT_CONTROLLER];
-      };
+    #endif
 
-  #endif
+    #if MODEL_EEPROM_EXTERNAL == ENUM_MODEL_EEPROM_EXTERNAL_24LCXXX
+        #include <I2C_eeprom.h> // https://github.com/RobTillaart/I2C_EEPROM
+    #endif
+
+    #if MODEL_TEMPERATURE_SENSOR == ENUM_MODEL_TEMPERATURE_SENSOR_PCT2075
+        #include <PCT2075.h> // https://github.com/jpliew/PCT2075
+
+        struct temperatureSensor{
+            uint8_t address = 0; /* I2C address. Default 0.*/
+            PCT2075 hardware = PCT2075(0); /* Reference to the hardware. */
+            float previousRead = 0; /* Previous read temperature. Default 0. */
+            unsigned long timePreviousRead = 0; /* Time (millis) when the sensor was last read. Default 0.*/
+            temperatureSensorLocation location;
+        };
+
+    #endif
+
 
 #endif
