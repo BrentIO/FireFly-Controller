5ddcfa4944b4335b940081020499b057028eadb1
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2023-02-19 20:07:37 -0500

    Changed outputs to class

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -8,8 +8,8 @@
 #include "common/frontPanel.h"
 
 
+managerOutputs outputs;
 managerInputs inputs;
-outputController outputControllers[COUNT_OUTPUT_CONTROLLER];
 managerTemperatureSensors temperatureSensors;
 managerFrontPanelButton frontPanelButton;
 
@@ -26,9 +26,11 @@ void setup() {
     frontPanelButton.begin();
 
     inputs.setCallback_failure(&handleInputFailure);
+    outputs.setCallback_failure(&outputFailure);
+    outputs.setCallback_publisher(&outputFailure);
+    outputs.begin();
     inputs.begin();
 
-    setupOutputs();
     temperatureSensors.setCallback_publisher(&temperaturePublisher);
     temperatureSensors.setCallback_failure(&temperatureFailure);
     temperatureSensors.begin();
@@ -40,36 +42,10 @@ void setup() {
 
 
 void loop() {
-
   inputs.loop();
   temperatureSensors.loop();
 
 }
-
-
-/**Instantiates the output objects*/
-void setupOutputs(){
-
-  //Setup output controllers
-  for(int i = 0; i < COUNT_OUTPUT_CONTROLLER; i++){
-    outputControllers[i].address = ADDRESSES_OUTPUT_CONTROLLER[i];
-
-    #if MODEL_OUTPUT_CONTROLLER == ENUM_MODEL_OUTPUT_CONTROLLER_PCA9685
-      outputControllers[i].hardware = PCA9685(outputControllers[i].address);
-      outputControllers[i].hardware.begin();
-      outputControllers[i].hardware.setFrequency(FREQUENCY_PWM);
-
-      //Ensure the controller is online
-      if(outputControllers[i].hardware.isConnected() == false){       
-        handleOutputFailure(&outputControllers[i]);
-      }
-
-    #endif
-
-  }
-
-}
-
 
 
 void temperaturePublisher(String location, float value){
