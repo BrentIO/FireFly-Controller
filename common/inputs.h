ca9d7a9a491c34207797c9039e6ede321e6bfdef
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2023-10-01 12:27:17 -0400

    Added documentation

--- common/inputs.h
+++ common/inputs.h
@@ -2,42 +2,88 @@
 
 class managerInputs{
 
+    /**
+     * Constants are defined in hardware.h
+     * 
+     * Definitions:
+     * 
+     *  -   Pin: Represents the input controller's physical pin connections to the motherboard
+     * 
+     *  -   Input Controller: Senses the state changes on a given pin.  When a state change is detected, the Input Controller makes the interrupt pin HIGH
+     * 
+     *  -   Interrupt Pin: The pin being monitored by the CPU for changes being reported by the Input Controller
+     * 
+     *  -   Port: RJ-45 port on the device, which is (typically) comprised of 4 input channels
+     * 
+     *  -   Channel: The wire within the RJ-45 port that has the ability to raise a signal high or low.  It is connected to a Pin on an Input Controller
+     * 
+    */
+
     public:
 
-        /** Collection of port and channel that is unique per controller. */
+        /** Collection of port and channel that is unique */
         struct portChannel{
             uint8_t port; /* RJ45 port */
             uint8_t channel; /* Wire within the RJ45 port */
         };
 
 
-        /* Failure reason codes, roughly based on i2c return messages. */
+        /** Failure reason codes, roughly based on i2c return messages */
         enum failureReason{
+            /// @brief Request was successful, no error was returned
             SUCCESS_NO_ERROR = 0, 
+
+            /// @brief Data too long to fit in transmit buffer
             DATA_TRANSMIT_BUFFER_ERROR = 1,
+
+            /// @brief Received NACK on transmit of address
             ADDRESS_OFFLINE = 2,
+
+            /// @brief Received NACK on transmit of data
             TRANSMIT_NOT_ACKNOLWEDGED = 3,
+
+            /// @brief Other error
             OTHER_ERROR = 4,
+
+            /// @brief Timeout
             TIMEOUT = 5,
+
+            /// @brief Invalid hardware configuration
             INVALID_HARDWARE_CONFIGURATION = 10,
+
+            /// @brief Unknown/undocumented failure
             UNKNOWN_ERROR = 11
         };
 
 
     private:
 
+        /** Input types */
         enum inputType{
+            /// @brief Input is normally open (typically a button)
             NORMALLY_OPEN = 0,
+
+            /// @brief Input is normally closed (typically a reed switch)
             NORMALLY_CLOSED = 1,
         };
 
 
+        /** Input states that can be observed */
         enum inputState{
+
+            /// @brief Input state is open/low.  This is not an indication of an abnormal state
             STATE_OPEN = LOW,
+
+            /// @brief Input state is closed/high.  This is not an indication of a normal state
             STATE_CLOSED = HIGH
         };
 
 
+        /** Input pin represents the individual input on the ioExtender.  Each pin on the ioExtender chip will have an inputPin associated.  
+         * 
+         * It contains current information, such as the time of last change, if the change has been handled (for both short and long changes), input type, and the current input state.
+         * The input pin can also be configured to monitor for long changes. 
+         * */
         struct inputPin{
             int64_t timeChange = 0; /* Time (milliseconds) when the input state last changed.  Value is set to 0 when the state returns to its input type. Default 0.*/
             boolean changeHandled = true; /* If the change has been handled, to make the event singular. Default true. */
@@ -45,10 +91,15 @@ class managerInputs{
             inputState state = STATE_OPEN; /* The state entered at timeChange. Default STATE_OPEN.*/
             inputType type = NORMALLY_OPEN; /* Defines if the input is normally open or normally closed. Default NORMALLY_OPEN.*/
             boolean monitorLongChange = false; /* Defines if the pin should be monitored for long changes.  Should be _true_ for buttons and _false_ for reed switches. Default false.*/
-
         };
 
 
+        /** ioExtender represents a physical integrated circuit chip installed on the hardware.
+         * 
+         * Each ioExtender is configured with an i2c address and interrupt pin.
+         * 
+         * It contains current information, such as the previous read value from the chip, if it is enabled or disabled, as well as contains a collection if inputPins.
+        */
         struct ioExtender{
             #if MODEL_IO_EXTENDER == ENUM_MODEL_IO_EXTENDER_PCA9995
                 PCA9555 hardware; /* Reference to the hardware. */
@@ -63,13 +114,26 @@ class managerInputs{
         };
 
 
+        /** Collection of input controllers (ioExtnder) attached to the motherboard */
         ioExtender inputControllers[COUNT_IO_EXTENDER];
+
+
+        /** Defines the port and channel for a given input controller */
         const uint8_t portChannelPinMap[COUNT_PINS_IO_EXTENDER][2] = IO_EXTENDER_CHANNELS;
 
+
+        /** Reference to the callback function that will be called when an input is sensed */
         void (*ptrPublisherCallback)(portChannel, boolean);
+
+
+        /** Reference to the callback function that will be called when an input controller has failed */
         void (*ptrFailureCallback)(uint8_t, failureReason);
 
-        /** Marks a given input controller as failed and raises a failed status */
+
+        /** Marks a given input controller as failed and raises a callback event, if configured 
+         * @param inputController The input controller being reported
+         * @param failureReason Reason code for the failure
+        */
         void failInputController(ioExtender *inputController, failureReason failureReason){
 
             if(inputController->enabled == false){
@@ -92,7 +156,9 @@ class managerInputs{
         }
 
 
-        /** Convert a bit to inputState */
+        /** Convert a bit to inputState 
+         * @param value As the value read from the hardware, where LOW/0 = CLOSED, and HIGH/1 = OPEN
+        */
         inputState bitToInputState(boolean value){
             if(value == LOW){
                 return inputState::STATE_CLOSED;
@@ -102,6 +168,10 @@ class managerInputs{
         }
 
 
+        /** Enumerates the i2c bus failure codes to a failureReason 
+         * @param i2cError The value returned from the i2c wire endTransmission() function
+         * @returns A failureReason enumeration mapped to the error code passed in
+        */
         failureReason i2cResponseToFailureReason(uint8_t i2cError){
 
             switch(i2cError){
@@ -292,20 +362,23 @@ class managerInputs{
 
     public:
 
+        /** Object containing the count of controllers and a list of each controller's bus status */
         struct healthResult{
-            uint8_t count = 0;
-            structHealth inputControllers[COUNT_IO_EXTENDER];
+            uint8_t count = 0; /** The number of input controllers */
+            structHealth inputControllers[COUNT_IO_EXTENDER]; /** Array of input controller health */
         };
 
-
+        /** Callback function that is called when an input is made */
         void setCallback_publisher(void (*userDefinedCallback)(portChannel, boolean)) {
             ptrPublisherCallback = userDefinedCallback; }
 
 
+        /** Callback function that is called when an input controller failure occurs */
         void setCallback_failure(void (*userDefinedCallback)(uint8_t, failureReason)) {
             ptrFailureCallback = userDefinedCallback; }
 
 
+        /** Initializes all input controllers */
         void begin(){
 
             if(this->_initialized == true){
@@ -327,7 +400,6 @@ class managerInputs{
                 failInputController(&invalid, failureReason::INVALID_HARDWARE_CONFIGURATION);
 
                 return;
-
             }
 
             if(COUNT_IO_EXTENDER != sizeof(addressesIoExtender)/sizeof(uint8_t)){
@@ -342,7 +414,6 @@ class managerInputs{
                 failInputController(&invalid, failureReason::INVALID_HARDWARE_CONFIGURATION);
 
                 return;
-
             }
 
             //Setup the input controllers
@@ -368,14 +439,14 @@ class managerInputs{
 
                 //Get the current input states
                 this->readInputPins(&this->inputControllers[i]);
-
             }
 
             this->_initialized = true;
-
         };
 
-        /** Returns the value of each input controller's bus status */
+        /** Get the health of the input controllers 
+         * @returns All input controllers and their bus state; the count of input controllers
+        */
         healthResult health(){
 
             healthResult returnValue;
@@ -394,6 +465,7 @@ class managerInputs{
             return returnValue;
         }
 
+        /** Observes changes in inputs with each main loop() cycle */
         void loop(){
 
             if(this->_initialized != true){
