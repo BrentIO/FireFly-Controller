467adfe04dc2fac9022ca89177b6cf4656a2bb0e
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2023-10-01 18:32:24 -0400

    Added support for failure reasons

--- common/outputs.h
+++ common/outputs.h
@@ -86,8 +86,9 @@ class managerOutputs{
 
         void (*ptrPublisherCallback)(void); //TODO: Determine correct signature
 
-        
-        void (*ptrFailureCallback)(void); //TODO: Determine correct signature
+
+        /** Reference to the callback function that will be called when an output controller has failed */
+        void (*ptrFailureCallback)(uint8_t, failureReason);
 
 
         /** Marks a given output controller as failed and raises a callback event, if configured 
@@ -116,6 +117,48 @@ class managerOutputs{
         }
 
 
+        /** Enumerates the i2c bus failure codes to a failureReason 
+         * @param i2cError The value returned from the i2c wire endTransmission() function
+         * @returns A failureReason enumeration mapped to the error code passed in
+        */
+        failureReason i2cResponseToFailureReason(uint8_t i2cError){
+
+            switch(i2cError){
+
+                case 0:
+                    return failureReason::SUCCESS_NO_ERROR;
+                    break;
+
+                case 1:
+                    return failureReason::DATA_TRANSMIT_BUFFER_ERROR;
+                    break;
+
+                case 2:
+                    return failureReason::ADDRESS_OFFLINE;
+                    break;
+
+                case 3:
+                    return failureReason::TRANSMIT_NOT_ACKNOLWEDGED;
+                    break;
+
+                case 4:
+                    return failureReason::OTHER_ERROR;
+                    break;
+
+                case 5:
+                    return failureReason::TIMEOUT;
+                    break;
+
+                case 10:
+                    return failureReason::INVALID_HARDWARE_CONFIGURATION;
+                    break;
+
+                default:
+                    return failureReason::UNKNOWN_ERROR;
+            }
+        }
+
+
     public:
 
         struct healthResult{
@@ -128,7 +171,8 @@ class managerOutputs{
             ptrPublisherCallback = userDefinedCallback; }
 
 
-        void setCallback_failure(void (*userDefinedCallback)(void)) {
+        /** Callback function that is called when an output controller failure occurs */
+        void setCallback_failure(void (*userDefinedCallback)(uint8_t, failureReason)) {
             ptrFailureCallback = userDefinedCallback; }
 
 
@@ -146,9 +190,10 @@ class managerOutputs{
                     Serial.println(F("[outputs] (begin) COUNT_OUTPUT_CONTROLLER and the length of ADDRESSES_OUTPUT_CONTROLLER are mismatched in hardware.h; Disabling outputs."));
                 #endif
 
-                if(this->ptrFailureCallback){
-                    this->ptrFailureCallback();
-                }
+                outputController invalid;
+                invalid.address = 0;
+
+                failOutputController(&invalid, failureReason::INVALID_HARDWARE_CONFIGURATION);
 
                 return;
 
@@ -160,18 +205,16 @@ class managerOutputs{
 
                 #if MODEL_OUTPUT_CONTROLLER == ENUM_MODEL_OUTPUT_CONTROLLER_PCA9685
                     this->outputControllers[i].hardware = PCA9685(outputControllers[i].address);
-                    this->outputControllers[i].hardware.begin();
+
+                    if(this->outputControllers[i].hardware.begin() == false){
+                        failOutputController(&this->outputControllers[i], i2cResponseToFailureReason(outputControllers[i].hardware.lastError()));
+                    };
+
                     this->outputControllers[i].hardware.setFrequency(FREQUENCY_PWM);
-
-
+                    
                     //Ensure the controller is online
-                    if(outputControllers[i].hardware.isConnected() == false){    
-                        this->outputControllers[i].enabled = false;
-
-                        if(this->ptrFailureCallback){
-                            this->ptrFailureCallback();
-                        }
-                           
+                    if(outputControllers[i].hardware.lastError() != 0){
+                        failOutputController(&this->outputControllers[i], i2cResponseToFailureReason(outputControllers[i].hardware.lastError()));
                     }
 
                 #endif
