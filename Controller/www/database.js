52cd0e41e0c92ca4a8b87f33ca7f603689463fd9
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2025-01-01 19:12:07 -0500

    WIP

--- Controller/www/database.js
+++ Controller/www/database.js
@@ -1,10 +1,15 @@
-let dbVersion = 2;
+let dbVersion = 3;
 
 const dbName = "FireFly-Controller";
 let db = new Dexie(dbName);
 
 
 const initDB = () => {
+    db.version(dbVersion).stores({
+        colors: "name", 
+        controllers: "++id, &uuid, name",
+        areas: "name"
+    })
     db.open();
 };
 
@@ -26,4 +31,82 @@ async function importConfig(file){
     } catch (error) {
         eventHandler(error);
     }
+}
+
+
+class controllerLocalStorage{
+    ip;
+    certificates = [];
+    softwareVersion;
+    uiVersion;
+
+    constructor(uuid){
+        this.uuid = uuid;
+        this.retrieve();
+    }
+
+    get ip(){
+        return this.ip;
+    }
+
+    set ip(value){
+        this.ip = value;
+    }
+
+    get certificates(){
+        return this.certificates;
+    }
+
+    set certificates(value){
+        this.certificates = value;
+    }
+
+    get softwareVersion(){
+        return this.softwareVersion;
+    }
+
+    set softwareVersion(value){
+        this.softwareVersion = value;
+    }
+
+    get uiVersion(){
+        return this.uiVersion;
+    }
+
+    set uiVersion(value){
+        this.uiVersion = value;
+    }
+
+    retrieve(){
+        const record = JSON.parse(localStorage.getItem(this.uuid));
+
+        if(record == null){
+            return;
+        }
+
+        if("ip" in record){
+            this.ip = record.ip;
+        }
+
+        if("certificates" in record){
+            this.certificates = record.certificates;
+        }
+
+        if("softwareVersion" in record){
+            this.softwareVersion = record.softwareVersion;
+        }
+
+        if("uiVersion" in record){
+            this.uiVersion = record.uiVersion;
+        }
+    }
+
+    save(){
+        localStorage.setItem(this.uuid, JSON.stringify(this));
+    }
+
+    delete(){
+        localStorage.removeItem(this.uuid);
+    }
+
 }
