b3e144ded6e37fc176482ada09aae74ab41f0047
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2025-01-19 18:29:46 -0500

    Add relays

--- Controller/www/database.js
+++ Controller/www/database.js
@@ -5,6 +5,7 @@ let db = new Dexie(dbName);
 
 let product_ids = [{"pid":"FFC3232-2305","inputs":{"count":32},"outputs":{"count":32}},{"pid":"FFC0806-2305","inputs":{"count":8},"outputs":{"count":6}}];
 let colors = [{"name":"Blue","hex":"#2a36e5"},{"name":"Green","hex":"#46a046"},{"name":"Red","hex":"#db1a1a"},{"name":"White","hex":"#ffffff"},{"name":"Yellow","hex":"#ffbc47"}];
+let relay_models = [{"type":"BINARY","manufacturer":"Crydom","model":"DR2260D20V","description":"Contactor"},{"type":"BINARY","manufacturer":"Crydom","model":"DR2220D20U","description":"Relay"},{"type":"VARIABLE","manufacturer":"Crydom","model":"PMP2425W","description":"Proportional Control"}];
 
 
 const initDB = () => {
@@ -15,7 +16,9 @@ const initDB = () => {
         product_ids: "pid",
         tags: "name",
         certificates: "++id, &certificate",
-        breakers: "++id, name"
+        breakers: "++id, name",
+        relays: "++id, name, breaker",
+        relay_models: "++id, [manufacturer+model], is_custom"
     });
 
     db.version(1).upgrade(function(transaction) {
@@ -26,6 +29,7 @@ const initDB = () => {
     db.on("populate", function(transaction) {
         transaction.product_ids.bulkAdd(product_ids);
         transaction.colors.bulkAdd(colors);
+        transaction.relay_models.bulkAdd(relay_models);
     });
 
     db.open();
