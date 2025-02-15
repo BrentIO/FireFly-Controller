f8c95650093e90c33f5013562d92e48834f39bb1
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2025-02-15 14:03:13 -0500

    Always generate UUIDs

--- Controller/www/common.js
+++ Controller/www/common.js
@@ -436,6 +436,20 @@ async function deleteUnusedCustomRelayModels(){
 }
 
 
+function randomUUID(){
+    if(typeof self.crypto.randomUUID == "function"){
+        return self.crypto.randomUUID();
+    }
+    
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    .replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0, 
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+
 function isValidUUID(value){
     var regex =/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
     return regex.test(value);
