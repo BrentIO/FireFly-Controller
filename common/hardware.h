4ee35c218833045ba914f0f41f98fa82e8c39670
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2025-03-29 08:00:21 -0400

    Changed port order to top left; Changed from channel 6 to channel 4 for UI consistency

--- common/hardware.h
+++ common/hardware.h
@@ -117,7 +117,7 @@
         #define IO_EXTENDER_MODEL ENUM_IO_EXTENDER_MODEL_PCA9995 /* IO Extender Model */
         #define IO_EXTENDER_COUNT 2 /* The number of IO extenders */
         #define IO_EXTENDER_ADDRESSES {0x20,0x21} /* I2C addresses for the IO extenders, order must match the PINS_INTERRUPT_IO_EXTENDER and should be sequential with the port numbers */
-        #define IO_EXTENDER_CHANNELS {{1,1},{1,2},{1,3},{2,6},{1,6},{2,3},{2,2},{2,1},{3,1},{3,2},{3,3},{4,6},{3,6},{4,3},{4,2},{4,1}} /* Physical mapping of the {port,channel} by pin */
+        #define IO_EXTENDER_CHANNELS {{2,1},{2,2},{2,3},{1,4},{2,4},{1,3},{1,2},{1,1},{4,1},{4,2},{4,3},{3,4},{4,4},{3,3},{3,2},{3,1}} /* Physical mapping of the {port,channel} by pin */
         #define IO_EXTENDER_INTERRUPT_PINS {34,35} /* Interrupt pins for the IO extenders, order must match the ADDRESSES_IO_EXTENDER */
         
         /* Output Controller */
