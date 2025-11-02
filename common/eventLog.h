c173fed6743f5ffd392c559d4733f1008d01520a
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2025-11-02 15:39:03 -0500

    Automatically log events to serial when an event is written

--- common/eventLog.h
+++ common/eventLog.h
@@ -114,6 +114,12 @@
                 newEvent.text = new char[EVENT_LOG_ENTRY_MAX_LENGTH + 1];
                 strcpy(newEvent.text, text);
 
+                if(newEvent.level == LOG_LEVEL_ERROR){
+                    log_e("New event log entry: [%s]", newEvent.text);
+                }else{
+                    log_d("New event log entry: [%s]", newEvent.text);
+                }
+
                 if(this->_eventLog.size() >= EVENT_LOG_MAXIMUM_ENTRIES){
                     this->_eventLog.shift();
                 }
