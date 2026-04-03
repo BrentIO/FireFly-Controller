6fb42fc5c344eec84b6b219ec7a42b5dd5666831
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2026-04-03 18:10:21 -0400

    Fix missed class

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -201,7 +201,7 @@ void setup() {
             deviceIdentity.data.product_hex,
             (uint32_t)PRODUCT_HEX);
       eventLog.createEvent(text, EventLog::LOG_LEVEL_ERROR);
-      oled.setPage(PAGE_ERROR);
+      oled.setPage(managerOled::PAGE_ERROR);
       log_e("Sleeping now");
       esp_deep_sleep_start();
     }
