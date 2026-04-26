2782e4229281337a4c0edd408eda09f26c223476
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2026-04-26 16:15:09 -0400

    Remove period from 'MAC Address is invalid' warning on /clients

--- Controller/ui/src/views/ClientsView.vue
+++ Controller/ui/src/views/ClientsView.vue
@@ -57,7 +57,7 @@
         </div>
         <p class="text-xs text-gray-500 dark:text-gray-400">{{ areaName(client.area) }}</p>
         <p class="text-xs text-gray-500 dark:text-gray-400">{{ hidSummary(client.hids) }}</p>
-        <p v-if="client.mac === 'ff:ff:ff:ff:ff:ff'" class="text-xs text-yellow-700 dark:text-yellow-500 mt-1 font-medium">MAC Address is invalid.</p>
+        <p v-if="client.mac === 'ff:ff:ff:ff:ff:ff'" class="text-xs text-yellow-700 dark:text-yellow-500 mt-1 font-medium">MAC Address is invalid</p>
       </div>
     </div>
 
