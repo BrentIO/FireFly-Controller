6419d21746efb0430e0df3b9b7448b032d5d579c
Brent Andrew Hendricks <brent.hendricks@p5software.com>
2026-04-26 15:40:14 -0400

    Change Variable relay type badge from purple to yellow

--- Controller/ui/src/views/config/RelayTypesView.vue
+++ Controller/ui/src/views/config/RelayTypesView.vue
@@ -29,7 +29,7 @@
             <td class="px-4 py-3">
               <span :class="r.type === 'BINARY'
                 ? 'px-2 py-0.5 text-xs rounded-full bg-blue-100 dark:bg-blue-900/40 text-blue-700 dark:text-blue-300'
-                : 'px-2 py-0.5 text-xs rounded-full bg-purple-100 dark:bg-purple-900/40 text-purple-700 dark:text-purple-300'">
+                : 'px-2 py-0.5 text-xs rounded-full bg-yellow-100 dark:bg-yellow-900/40 text-yellow-700 dark:text-yellow-300'">
                 {{ r.type === 'BINARY' ? 'Binary' : 'Variable' }}
               </span>
             </td>
